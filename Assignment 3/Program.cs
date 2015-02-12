7a6a1b9dbf21084d61e8185e500faba5fcbbb6f4
keshok <keshok@live.ca>
2015-02-11 21:40:34 -0500

    Added Name Strings

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -11,11 +11,18 @@ namespace Assignment_3
         static void Main(string[] args)
         {
             string prompt;
+            string name;
+            string name2;
             int restaurantMenu = 0;
 
             Console.WriteLine("*******************************************************************************");
             Console.WriteLine("*******************************************************************************");
             Console.WriteLine("This program will demonstrate a case statement and a while loop");
+            Console.WriteLine("*******************************************************************************");
+            Console.Write("Enter your first name: ");
+            name = Console.ReadLine();
+            Console.Write("Enter your last name: ");
+            name2 = Console.ReadLine();
             Console.WriteLine("*******************************************************************************");
             Console.WriteLine("What would you like to eat on the menu?");
             while(restaurantMenu != 5)
@@ -27,39 +34,39 @@ namespace Assignment_3
                 Console.WriteLine("4) Ice Cream");
                 Console.WriteLine("5) Nothing");
                 Console.WriteLine();
-                Console.Write("Please make your selection (1 to 5): ");
+                Console.Write("{0} {1}, please make your selection (1 to 5): ", name, name2);
                 prompt = Console.ReadLine();
                 restaurantMenu = Convert.ToInt32(prompt);
                 switch (restaurantMenu)
                 {
                     case 1:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("You want to order pizza?");
-                        Console.WriteLine("What else would you like on the menu?");
+                        Console.WriteLine("Do you want to order pizza?");
+                        Console.WriteLine("What else would you like to order on the menu {0}?", name);
                         break;
                     case 2:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("You want to order burger?");
-                        Console.WriteLine("What else would you like on the menu?");
+                        Console.WriteLine("Do you want to order burger(s)?");
+                        Console.WriteLine("What else would you like to order on the menu {0}?", name);
                         break;
                     case 3:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("You want to order hot dog?");
-                        Console.WriteLine("What else would you like on the menu?");
+                        Console.WriteLine("Do you want to order hot dog(s)?");
+                        Console.WriteLine("What else would you like to order on the menu {0}?", name);
                         break;
                     case 4:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("You want to order ice cream?");
-                        Console.WriteLine("What else would you like on the menu?");
+                        Console.WriteLine("Do you want to order ice cream?");
+                        Console.WriteLine("What else would you like to order on the menu {0}?", name);
                         break;
                     case 5:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("You don't want anything?");
-                        Console.WriteLine("Thank you for ordering and have a nice day! :)");
+                        Console.WriteLine("You don't want to order anything?");
+                        Console.WriteLine("Thank you for ordering and have a nice day {0}! :)", name);
                         break;
                     default:
                         Console.WriteLine("*******************************************************************************");
-                        Console.WriteLine("ERROR - Invalid choice or selction, please try again!");
+                        Console.WriteLine("ERROR - Invalid choice or selction, please try again {0}!", name);
                         break;
                 }
             }
