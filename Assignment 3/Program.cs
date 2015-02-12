b3e472eff982038b457bbfc9bd1d502f0c1790a2
keshok <keshok@live.ca>
2015-02-11 20:50:51 -0500

    Menu Restaurant Complete

--- Assignment 3/Program.cs
+++ Assignment 3/Program.cs
@@ -10,6 +10,27 @@ namespace Assignment_3
     {
         static void Main(string[] args)
         {
+            string prompt;
+            int restaurantMenu;
+            bool choice;
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("What would you like to eat on the menu?");
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("1) Pizza");
+            Console.WriteLine("2) Burger");
+            Console.WriteLine("3) Hot Dog");
+            Console.WriteLine("4) Ice Cream");
+            Console.WriteLine("5) Exit");
+            Console.WriteLine();
+            Console.Write("Please make your selection (1 to 5): ");
+            prompt = Console.ReadLine();
+            restaurantMenu = Convert.ToInt32(prompt);
+
+            Console.WriteLine("*******************************************************************************");
+            Console.WriteLine("Press any key to continue...");
+            Console.WriteLine("*******************************************************************************");
+            Console.ReadKey();
         }
     }
 }
